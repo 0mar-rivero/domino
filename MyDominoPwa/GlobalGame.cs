e07d6ae19f791518f6bcefe115cb7934eb920ce1
asciber45 <alex.sierra@estudiantes.matcom.uh.cu>
2022-07-16 20:22:36 -0400

    null

--- MyDominoPwa/GlobalGame.cs
+++ MyDominoPwa/GlobalGame.cs
@@ -3,18 +3,18 @@
 namespace MyDominoPwa;
 
 public class GlobalState<TDominoType> {
-	public GameState<int> GlobalGameState { get; set; }
+	public GameState<int>? GlobalGameState { get; set; }
 	public Game<int> GlobalGame { get; set; } = new();
-	public Tournament<int> GLobalTournament { get; set; }
+	public Tournament<int>? GLobalTournament { get; set; }
 	public bool Visible { get; internal set; } = false;
 
-	public IMatcher<TDominoType> Matcher { get; set; }
-	public IScorer<TDominoType> Scorer { get; set; }
-	public ITurner<TDominoType> Turner { get; set; }
-	public IDealer<TDominoType> Dealer { get; set; }
-	public IGenerator<TDominoType> Generator { get; set; }
-	public IFinisher<TDominoType> Finisher {get; set; }
+	public IMatcher<TDominoType>? Matcher { get; set; }
+	public IScorer<TDominoType>? Scorer { get; set; }
+	public ITurner<TDominoType>? Turner { get; set; }
+	public IDealer<TDominoType>? Dealer { get; set; }
+	public IGenerator<TDominoType>? Generator { get; set; }
+	public IFinisher<TDominoType>? Finisher {get; set; }
 
 	public Dictionary<int, Team<int>> Teams { get; set; } = new();
-	public Judge<int> Judge { get; set; }
+	public Judge<int>? Judge { get; set; }
 }
